8e023f2b206e5c254a94c6c55bfc44113b78593c
Elena Ordina <post032@yandex.ru>
2020-10-01 21:57:14 +0300

    dom

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 'use strict';
 
-let checkinArr = [`12:00`, `13:00`, `14:00`];
-let checkoutArr = [`12:00`, `13:00`, `14:00`];
-let featuresArr = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
-let typeArr = [`palace`, `flat`, `house`, `bungalow`];
-let photosArr = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
+const CHECKOUT_ARR = [`12:00`, `13:00`, `14:00`];
+const CHECKIN_ARR = [`12:00`, `13:00`, `14:00`];
+const FEATURES_ARR = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
+const TYPE_ARR = [`palace`, `flat`, `house`, `bungalow`];
+const PHOTOS_ARR = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
 document.querySelector(`.map`).classList.remove(`map--faded`);
 
 function getRandomInt(min, max) {
@@ -29,44 +29,42 @@ function getRandomArray(array) {
 let newAds = [];
 
 for (let i = 0; i < 8; i++) {
-  let location = {
+  let locationPoint = {
     x: getRandomInt(0, 1200),
     y: getRandomInt(130, 630)
   };
   let newObjectAd = {
     author: {
-      avatar: `img/avatars/user0` + (i + 1) + `.png`,
+      avatar: `img/avatars/user0${i + 1}.png`,
     },
     offer: {
-      title: `Заголовок ` + (i + 1),
-      address: location,
+      title: `Заголовок${i + 1}`,
+      address: locationPoint,
       price: getRandomInt(100, 6000),
-      type: getRandomElement(typeArr),
+      type: getRandomElement(TYPE_ARR),
       rooms: getRandomInt(1, 4),
       guests: getRandomInt(1, 6),
-      checkin: getRandomElement(checkinArr),
-      checkout: getRandomElement(checkoutArr),
-      features: getRandomArray(featuresArr),
+      checkin: getRandomElement(CHECKIN_ARR),
+      checkout: getRandomElement(CHECKOUT_ARR),
+      features: getRandomArray(FEATURES_ARR),
       description: `Здесь будет ваше описание`,
-      photos: getRandomArray(photosArr),
+      photos: getRandomArray(PHOTOS_ARR),
     },
-    location: {
-      x: location.x,
-      y: location.y,
-    }
+    location: locationPoint,
   };
   newAds.push(newObjectAd);
 }
 
 let markElementTemplate = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
-let markElements = document.querySelector(`.map__pin`);
+let markElements = document.querySelector(`.map`);
 
 let renderMark = function (newAd) {
   let markElement = markElementTemplate.cloneNode(true);
   markElement.style.left = newAd.location.x + 32 + `px`;
   markElement.style.top = newAd.location.y + 64 + `px`;
-  markElement.querySelector(`img`).setAttribute(`src`, newAd.author.avatar);
-  markElement.querySelector(`img`).setAttribute(`alt`, `фото пользователя`);
+  let avatarImg = markElement.querySelector(`img`);
+  avatarImg.src = newAd.author.avatar;
+  avatarImg.alt = `фото пользователя`;
   return markElement;
 };
 
